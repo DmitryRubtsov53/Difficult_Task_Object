b2b38845f32a375b7a4e03ea9c6afcd695f6f0f4
DmitryRubtsov53 <2578390@mail.ru>
2022-09-30 14:47:25 +0500

    53

---
+++ src/Middle/Book.java
@@ -0,0 +1,21 @@
+package Middle;
+
+public class Book {
+    private final String bookName;
+    private final int yearPublishing;
+    private final String autor;
+
+    public  String getBookName() {
+        return this.bookName;
+    }
+    public String getAutor() { return this.autor; }
+
+    public int getYearPublishing() { return yearPublishing; }
+
+
+    public Book (String bookName, String autor, int yearPublishing) {
+        this.bookName = bookName;
+        this.autor = autor;
+        this.yearPublishing = yearPublishing;
+    }
+}
---
+++ src/Middle/Main.java
@@ -0,0 +1,48 @@
+package Middle;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Book [] books = new Book[11];
+
+        books[0] = new Book("Капитал", "Карл Маркс" , 1885);
+        books[1] = new Book("Война и мир", "Лев Толстой" , 1970);
+        books[2] = new Book("Мартин Иден", "Джек Лондон" , 1985);
+        books[3] = new Book("Три товарища", "Э.М. Ремарк" , 1997);
+        books[4] = new Book("Старик и море", "Э. Хеменгуэй" , 1992);
+        books[5] = new Book("Алые паруса", "Александр Грин" , 1968);
+        books[6] = new Book(null, null , 0);
+        books[7] = new Book(null, null , 0);
+        books[8] = new Book(null, null , 0);
+        books[9] = new Book(null, null , 0);
+        books[10] = new Book(null, null , 0);
+
+        Book [] bookNew = new Book[11];
+        int j= 0;
+        bookNew[0] = new Book("Конвой PQ-17", "Валентин Пикуль" , 1995);
+        bookNew[1] = new Book("Революция", "Владимир Ленин" , 1921);
+        bookNew[2] = new Book("Тихий Дон", "Михаил Шолохов" , 1985);
+        bookNew[3] = new Book("Живые и мертвые", "К. Симонов" , 1997);
+        bookNew[4] = new Book("Огнём и мечём", "Г. Сенкевич" , 1992);
+
+
+
+//    public static Book[] makeLibrary(books) {
+
+        //Book bookNull = new Book(null, null , 0);
+        //Book bookNew = new Book("Конвой PQ-17", "В. Пикуль" , 1995);
+        // Находим своболную ячейку массива:
+        for (int i = 0; i < books.length; i++) {
+            if (books[i].getBookName() == null) {
+                books[i] = bookNew[j]; //new Book("Конвой PQ-17", "Валентин Пикуль" , 1995);;
+                j++; //break;
+            }
+        }
+        for (Book element : books) {
+            System.out.println(element.getBookName() + " : " + element.getAutor() + " : " + element.getYearPublishing() + ".");
+            }
+
+
+    }
+
+}
