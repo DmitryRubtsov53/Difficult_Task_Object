0543bb233edb540db290306c5623868ef6915dd3
DmitryRubtsov53 <2578390@mail.ru>
2022-09-30 22:15:43 +0500

    Решил задачу ПС Среднего уровня к Уроку Объекты и классы.

--- src/Middle/Main.java
+++ src/Middle/Main.java
@@ -3,7 +3,7 @@ package Middle;
 public class Main {
     public static void main(String[] args) {
 
-        Book [] books = new Book[11];
+        Book [] books = new Book[11];  // Исходный список книг
 
         books[0] = new Book("Капитал", "Карл Маркс" , 1885);
         books[1] = new Book("Война и мир", "Лев Толстой" , 1970);
@@ -17,32 +17,35 @@ public class Main {
         books[9] = new Book(null, null , 0);
         books[10] = new Book(null, null , 0);
 
-        Book [] bookNew = new Book[11];
-        int j= 0;
+        Book [] bookNew = new Book[11];   // Добавочный список книг
+
         bookNew[0] = new Book("Конвой PQ-17", "Валентин Пикуль" , 1995);
         bookNew[1] = new Book("Революция", "Владимир Ленин" , 1921);
         bookNew[2] = new Book("Тихий Дон", "Михаил Шолохов" , 1985);
         bookNew[3] = new Book("Живые и мертвые", "К. Симонов" , 1997);
         bookNew[4] = new Book("Огнём и мечём", "Г. Сенкевич" , 1992);
 
+        books = addBooks ( books, bookNew);
 
-
-//    public static Book[] makeLibrary(books) {
-
-        //Book bookNull = new Book(null, null , 0);
-        //Book bookNew = new Book("Конвой PQ-17", "В. Пикуль" , 1995);
-        // Находим своболную ячейку массива:
-        for (int i = 0; i < books.length; i++) {
-            if (books[i].getBookName() == null) {
-                books[i] = bookNew[j]; //new Book("Конвой PQ-17", "Валентин Пикуль" , 1995);;
-                j++; //break;
-            }
-        }
-        for (Book element : books) {
-            System.out.println(element.getBookName() + " : " + element.getAutor() + " : " + element.getYearPublishing() + ".");
-            }
-
+        printListBooks (books);
 
     }
+        public static Book[] addBooks(Book[] books, Book[] bookNew) {
+            int j = 0;
+            // Находим своболную ячейку массива и записываем новую книгу:
+            for (int i = 0; i < books.length; i++) {
+                if (books[i].getBookName() == null) {
+                    books[i] = bookNew[j];
+                    j++;
+                }
+            }
+            return books;
+        }
+        public static void printListBooks (Book[] books) {
 
-}
+            for (Book element : books) {
+                System.out.println(element.getBookName() + " : " + element.getAutor() + " : " + element.getYearPublishing() + ".");
+            }
+        }
+
+}  // скобка класса
